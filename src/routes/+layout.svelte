4a175da5336a893592f72523271d014c20e0d5c3
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-15 08:15:10 +0200

    :sparkles: Default layout and home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+  // The ordering of these imports is critical to your app working properly
+  import "@skeletonlabs/skeleton/themes/theme-modern.css";
+  // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+  import "@skeletonlabs/skeleton/styles/skeleton.css";
+  // Most of your app wide CSS should be put in this file
+  import "../app.postcss";
+  import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+</script>
+
+<!-- App Shell -->
+<AppShell>
+  <svelte:fragment slot="header">
+    <!-- App Bar -->
+    <AppBar class="rounded-3xl">
+      <svelte:fragment slot="lead">
+        <a href="/" class="flex items-center"
+          ><img
+            src="/logo.png"
+            alt="PerformSquad Logo"
+            class="aspect-auto h-10 m-2"
+          />
+          <strong class="text-xl uppercase">PerformSquad</strong></a
+        >
+      </svelte:fragment>
+      <svelte:fragment slot="trail">
+        <a class="btn btn-sm variant-ghost-surface" href="/afiliacion">
+          Afiliarse
+        </a>
+        <a class="btn btn-sm variant-ghost-surface" href="/login"> Login </a>
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+  <!-- Page Route Content -->
+  <div class="rounded-3xl p-4">
+    <slot />
+  </div>
+</AppShell>

461d26f6292908ffbc3df51e12a8a320d754c6be
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-15 08:25:32 +0200

    :sparkles: Login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,55 @@
+<svelte:head>
+  <title>Login</title>
+</svelte:head>
+
+<div class="w-full text-token grid grid-cols-2 md:grid-cols-3 gap-8">
+  <a class="card card-hover overflow-hidden" href="afiliacion">
+    <header>
+      <img
+        src="/afiliacion.jpg"
+        class="bg-black/33 w-full aspect-[16/9] filter grayscale hover:grayscale-0 transition duration-500 ease-in-out hover:filter-none"
+        alt="Post"
+      />
+    </header>
+    <div class="p-4 space-y-4 text-center">
+      <h3 class="h3" data-toc-ignore>Afiliación</h3>
+    </div>
+    <hr class="opacity-50" />
+  </a><a class="card card-hover overflow-hidden" href="login/arbitro">
+    <header>
+      <img
+        src="/arbitro.jpg"
+        class="bg-black/33 w-full aspect-[16/9] filter grayscale hover:grayscale-0 transition duration-500 ease-in-out hover:filter-none"
+        alt="Post"
+      />
+    </header>
+    <div class="p-4 space-y-4 text-center">
+      <h3 class="h3" data-toc-ignore>Árbitro</h3>
+    </div>
+    <hr class="opacity-50" />
+  </a><a class="card card-hover overflow-hidden" href="login/club">
+    <header>
+      <img
+        src="/club.jpg"
+        class="bg-black/33 w-full aspect-[16/9] filter grayscale hover:grayscale-0 transition duration-500 ease-in-out hover:filter-none"
+        alt="Post"
+      />
+    </header>
+    <div class="p-4 space-y-4 text-center">
+      <h3 class="h3" data-toc-ignore>Club</h3>
+    </div>
+    <hr class="opacity-50" />
+  </a><a class="card card-hover overflow-hidden" href="login/federacion">
+    <header>
+      <img
+        src="/federation.jpg"
+        class="bg-black/33 w-full aspect-[16/9] filter grayscale hover:grayscale-0 transition duration-500 ease-in-out hover:filter-none"
+        alt="Post"
+      />
+    </header>
+    <div class="p-4 space-y-4 text-center">
+      <h3 class="h3" data-toc-ignore>Federación</h3>
+    </div>
+    <hr class="opacity-50" />
+  </a>
+</div>

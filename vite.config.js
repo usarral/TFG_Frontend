4da95b999a9381b4c1f6303a555008cfdf465ab1
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-16 21:36:30 +0200

    :wrench: Use https on dev

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), mkcert()],
   server: {
-    port: 80,
-    strictPort: false
+    port: 443,
+    strictPort: false,
+    https: true
   }
 })

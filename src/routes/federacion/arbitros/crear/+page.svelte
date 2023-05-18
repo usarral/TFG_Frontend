0f15b874272ab2bd089653c861da0320dc2400d2
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-18 13:14:07 +0200

    :sparkles: Working crear arbitro

--- src/routes/federacion/arbitros/crear/+page.svelte
+++ src/routes/federacion/arbitros/crear/+page.svelte
@@ -1,49 +1,128 @@
+<script>
+  const handleOnSubmit = (e) => {
+    const formData = new FormData(e.target);
+    const data = [];
+    for (let field of formData) {
+      const [key, value] = field;
+      data.push({ key, value });
+    }
+    // Cuando tenemos los datos, data es un array de objetos, cada objeto tiene una key y un value
+    //para enviarlo a la API, tenemos que convertirlo a un objeto con la key y el value
+    const dataObject = data.reduce((acc, { key, value }) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+    sendForm(dataObject);
+  };
+
+  const sendForm = async (data) => {
+    const response = await fetch("http://localhost:3000/arbitro", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const res = await response.json();
+    console.log(res);
+  };
+</script>
+
 <svelte:head>
   <title>Crear Arbitro - PerformSquad</title>
 </svelte:head>
 
-<form action="">
+<form on:submit|preventDefault={handleOnSubmit}>
   <h1 class="text-4xl text-center py-8">Nuevo Arbitro</h1>
 
   <div class="flex flex-row gap-8 w-full">
     <label class="label text-left py-4 w-full">
       <span>Nombre *</span>
-      <input class="input" type="text" placeholder="Nombre" required />
+      <input
+        class="input"
+        type="text"
+        name="nombre"
+        placeholder="Nombre"
+        id="nombre"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Apellido *</span>
-      <input class="input" type="text" placeholder="Apellido" required />
+      <input
+        class="input"
+        type="text"
+        name="apellido"
+        placeholder="Apellido"
+        id="apellido"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Segundo Apellido</span>
-      <input class="input" type="text" placeholder="Segundo Apellido" />
+      <input
+        class="input"
+        type="text"
+        id="apellido2"
+        name="apellido2"
+        placeholder="Segundo Apellido"
+      />
     </label>
   </div>
   <div class="flex flex-row gap-8 w-full">
     <label class="label text-left py-4 w-full">
       <span>DNI *</span>
-      <input class="input" type="text" placeholder="DNI" required />
+      <input
+        class="input"
+        type="text"
+        name="DNI"
+        placeholder="DNI"
+        id="dni"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Teléfono *</span>
-      <input class="input" type="text" placeholder="Teléfono" required />
+      <input
+        class="input"
+        type="text"
+        name="telefono"
+        id="telefono"
+        placeholder="Teléfono"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Email *</span>
-      <input class="input" type="email" placeholder="Email" required />
+      <input
+        class="input"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Email"
+        required
+      />
     </label>
   </div>
   <div class="flex flex-row gap-8 w-full">
     <label class="label text-left py-4 w-full">
       <span>Fecha Nacimiento *</span>
-      <input class="input" type="date" required />
+      <input
+        class="input"
+        name="fechaNacimiento"
+        type="date"
+        id="fecha"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Dirección *</span>
       <input
         class="input"
         type="text"
+        name="direccion"
         placeholder="Ej: Calle Mayor 27"
+        id="direccion"
         required
       />
     </label>
@@ -51,15 +130,35 @@
   <div class="flex flex-row gap-8 w-full">
     <label class="label text-left py-4 w-full">
       <span>Ciudad *</span>
-      <input class="input" type="text" placeholder="Ciudad" required />
+      <input
+        class="input"
+        type="text"
+        name="ciudad"
+        id="ciudad"
+        placeholder="Ciudad"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Provincia *</span>
-      <input class="input" type="text" placeholder="Apellido" required />
+      <input
+        class="input"
+        type="text"
+        name="provincia"
+        id="provincia"
+        placeholder="Provincia"
+        required
+      />
     </label>
     <label class="label text-left py-4 w-full">
       <span>Código Postal *</span>
-      <input class="input" type="number" placeholder="Ej: 50001" />
+      <input
+        class="input"
+        type="number"
+        name="cp"
+        id="cp"
+        placeholder="Ej: 50001"
+      />
     </label>
   </div>
   <div class="text-center">

9cb2c93b1c72c8588849cdd1f4057f7d9e29d7ec
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-06-01 09:58:58 +0200

    :sparkles: Club partidos page

--- src/routes/club/partidos/+page.svelte
+++ src/routes/club/partidos/+page.svelte
@@ -1,47 +1,83 @@
 <script>
   import { Table, tableMapperValues } from "@skeletonlabs/skeleton";
   import { onMount } from "svelte";
-
-  let data = []; // Variable para almacenar los datos de la API
+  import { checkAuth } from "$functions/checkAuth";
+  checkAuth();
+  let data = [];
 
   onMount(async () => {
-    // vecomo
-    // get domain from url
+    const getEquipo = async (id) => {
+      const domain = window.location.hostname;
+      try {
+        const response = await fetch(`http://${domain}:3000/equipo/${id}`);
+        if (response.ok) {
+          const data = await response.json();
+          return data.data.nombre;
+        } else {
+          console.error(
+            "Error al obtener los datos de la API:",
+            response.status
+          );
+        }
+      } catch (error) {
+        console.error("Error al obtener los datos de la API:", error);
+      }
+    };
+    const getPabellon = async (id) => {
+      const domain = window.location.hostname;
+      try {
+        const response = await fetch(`http://${domain}:3000/pabellon/${id}`);
+        if (response.ok) {
+          const data = await response.json();
+          return data.data.nombre;
+        } else {
+          console.error(
+            "Error al obtener los datos de la API:",
+            response.status
+          );
+        }
+      } catch (error) {
+        console.error("Error al obtener los datos de la API:", error);
+      }
+    };
     const domain = window.location.hostname;
     try {
-      const response = await fetch(`http://${domain}:3000/jugador`);
-      if (response.ok) {
+      const response = await fetch(
+        `http://${domain}:3000/partido?club=${localStorage.getItem("club")}`
+      );
+      if (response.status == 204) {
+        return;
+      } else if (response.ok) {
         data = await response.json();
         const $ = (selector) => document.querySelector(selector);
         const table = $(".table");
-        data.data.forEach((jugador) => {
+
+        data.data.forEach(async (partido) => {
+          const equipoLocal = await getEquipo(partido.equipoLocal);
+          const equipoVisitante = await getEquipo(partido.equipoVisitante);
+          const pabellon = await getPabellon(partido.pabellon);
           table.innerHTML += `<tr>
-            <td><img
-loading="lazy" class="rounded-full" src="${jugador.foto}"></td>
-                  <td>${
-                    jugador.apellido2 != null
-                      ? jugador.apellido +
-                        " " +
-                        jugador.apellido2 +
-                        ", " +
-                        jugador.nombre
-                      : jugador.apellido1 + ", " + jugador.nombre
-                  }</td>
-                  <td>${
-                    //calcular edad
-                    new Date().getFullYear() -
-                    new Date(jugador.fechaNacimiento).getFullYear()
-                  }</td>
-                  <td>${jugador.DNI}</td> 
-                  <td>${jugador.email}</td>  
-                  <td style="display: flex;flex-direction: column;">
-                      <a href="/federacion/categorias/editar/${
-                        jugador.id
-                      }" class="btn btn-sm variant-primary">Editar</a>
-                      <a href="/federacion/categorias/borrar/${
-                        jugador.id
-                      }" class="btn btn-sm variant-danger">Borrar</a>
-                      </td>`;
+            <td>${new Date(partido.fecha).toLocaleDateString()} 
+              ${
+                new Date(partido.fecha).getHours() < 10
+                  ? "0" + new Date(partido.fecha).getHours()
+                  : new Date(partido.fecha).getHours()
+              }:${
+            new Date(partido.fecha).getMinutes() < 10
+              ? "0" + new Date(partido.fecha).getMinutes()
+              : new Date(partido.fecha).getMinutes()
+          }</td>
+            <td>${equipoLocal}</td>
+            <td>${equipoVisitante}</td>
+            <td>${pabellon}</td>
+            <td style="display: flex;flex-direction: column;">
+                <a href="/federacion/partidos/editar/${
+                  partido.id
+                }" class="btn btn-sm variant-primary">Editar</a>
+                <a href="/federacion/partidos/borrar/${
+                  partido.id
+                }" class="btn btn-sm variant-danger">Borrar</a>
+                </td>`;
         });
       } else {
         console.error("Error al obtener los datos de la API:", response.status);
@@ -51,21 +87,27 @@ loading="lazy" class="rounded-full" src="${jugador.foto}"></td>
     }
   });
   const tableSimple = {
-    head: ["Foto", "Nombre", "Fecha de nacimiento", "DNI", "Email", "Acciones"], //Pasar datos a la tabla aqui
+    head: [
+      "Fecha del encuentro",
+      "Equipo local",
+      "Equipo visitante",
+      "Pabellón",
+      "Acciones",
+    ],
     body: tableMapperValues(data),
   };
 </script>
 
 <svelte:head>
-  <title>Gestión de Partidos - PerformSquad</title>
+  <title>Gestión de partidos - PerformSquad</title>
 </svelte:head>
 
 <div class="flex flex-col gap-8">
-  <h1 class="text-4xl text-center py-8">Gestión de Partidos</h1>
+  <h1 class="text-4xl text-center py-8">Gestión de partidos</h1>
   <Table source={tableSimple} />
   <div class="text-center">
     <a
-      href="/federacion/jugadors/crear"
+      href="/federacion/partidos/crear"
       class="
           btn
           variant-filled-primary
@@ -74,7 +116,7 @@ loading="lazy" class="rounded-full" src="${jugador.foto}"></td>
           w-80
           "
     >
-      Nuevo jugador
+      Nuevo partido
     </a>
   </div>
 </div>

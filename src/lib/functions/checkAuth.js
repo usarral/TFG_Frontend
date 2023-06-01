7bb7a050253c8db5db880908df860dd2899a9638
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-06-01 19:18:44 +0200

    ✨ Check Auth Features

--- src/lib/functions/checkAuth.js
+++ src/lib/functions/checkAuth.js
@@ -1,65 +1,57 @@
 import { getCurrentBrowserFingerPrint } from '@rajesh896/broprint.js'
 import { onMount } from 'svelte'
 export async function checkAuth () {
+  //Get the fingerprint of the current browser and store it in a variable called fingerprint for access on OnMount
   onMount(async () => {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
+    let fingerprint = await getCurrentBrowserFingerPrint()
+    // Obten las cookies
+    let cookies = document.cookie.split(';')
+    if (!cookies) {
+      window.location.href = '/login'
+      return
+    }
+    // Busca la cookie que contiene el token
+    let tokenCookie = cookies.find(cookie => cookie.includes('token'))
+    if (!tokenCookie) {
+      window.location.href = '/login'
+      return
+    }
+    // Extrae el token de la cookie
+    let token = tokenCookie.split('=')[1]
+
+    if (!token) {
+      window.location.href = '/login'
+      return
+    }
+
+    // Comprueba que el token sea válido
+    let response = await fetch(
+      `${import.meta.env.VITE_BACKEND_URL}/auth/check`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token, fingerprint })
       }
-    })
-    const data = await response.json()
-    console.log(data)
+    )
+    let data = await response.json()
+
+    if (data.message === 'Token valido') {
+      localStorage.setItem('rol', data.data.rol)
+      localStorage.setItem('id', data.data.id)
+      localStorage.setItem('nombre', data.data.nombre)
+      localStorage.setItem('arbitro', data.data.arbitro)
+      localStorage.setItem('club', data.data.club)
+    } else {
+      document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+      window.location.href = '/login'
+      return
+    }
+    const url = window.location.pathname.split('/')[1]
+    if (url !== data.data.rol) {
+      window.location.href = `/${data.data.rol}`
+      return
+    }
   })
-  // // Get the browser fingerprint
-  // getCurrentBrowserFingerPrint().then(fingerprint => {
-  //   document
-  //     .getElementById('browser-fingerprint')
-  //     .setAttribute('data-value', fingerprint)
-  //   return fingerprint
-  // })
-  // onMount(async () => {
-  //   const fingerprint = document
-  //     .getElementById('browser-fingerprint')
-  //     .getAttribute('data-value')
-  //   const token = localStorage.getItem('token')
-  //   if (token) {
-  //     const response = await fetch(`${process.env.API_URL}/user/checkauth`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: `Bearer ${token}`
-  //       },
-  //       body: JSON.stringify({ fingerprint: await fingerprint })
-  //     })
-  //     const data = await response.json()
-  //     // if response is 200 then user is authenticated
-  //     if (data.status === 'success') {
-  //       console.log('User is authenticated')
-  //       return true
-  //     }
-  //     console.log('User is not authenticated')
-  //     window.location.href = '/login'
-  //     return false
-  //   } else {
-  //     console.log('User is not authenticated')
-  //     window.location.href = '/login'
-  //     return false
-  //   }
-  //   // const response = await fetch('http://localhost:3000/user/checkauth', {
-  //   //   method: 'POST',
-  //   //   headers: {
-  //   //     'Content-Type': 'application/json'
-  //   //   },
-  //   //   body: JSON.stringify({ fingerprint: await fingerprint })
-  //   // })
-  //   // const data = await response.json()
-  //   // if (data.status === 'success') {
-  //   //   console.log('User is authenticated')
-  //   //   return true
-  //   // }
-  //   // console.log('User is not authenticated')
-  //   // window.location.href = '/login'
-  //   // return false
-  // })
 }

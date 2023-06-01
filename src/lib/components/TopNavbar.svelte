30e468ea275f44ac7ecf60162a54d4bbe9af63a5
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-06-01 19:18:17 +0200

    ✨ Login and logout buttons

--- src/lib/components/TopNavbar.svelte
+++ src/lib/components/TopNavbar.svelte
@@ -1,11 +1,33 @@
 <script>
   import { AppBar } from "@skeletonlabs/skeleton";
   import { onMount } from "svelte";
+  const logout = async () => {
+    await fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.href = "/";
+    localStorage.clear();
+  };
   onMount(() => {
     const domain = window.location.host;
     const $ = (selector) => document.querySelector(selector);
     const aHome = $("#aHome");
     aHome.href = `http://${domain}/`;
+    const nombre = $("#nombreUsuario");
+    const guest = $("#guest");
+    const logged = $("#logged");
+    if (localStorage.getItem("nombre")) {
+      nombre.textContent = localStorage.getItem("nombre");
+      guest.classList.add("hidden");
+      logged.classList.remove("hidden");
+    } else {
+      guest.classList.remove("hidden");
+      logged.classList.add("hidden");
+    }
   });
 </script>
 
@@ -22,9 +44,18 @@
     >
   </svelte:fragment>
   <svelte:fragment slot="trail">
-    <a class="btn btn-sm variant-ghost-surface" href="/afiliacion">
-      Afiliarse
-    </a>
-    <a class="btn btn-sm variant-ghost-surface" href="/login"> Login </a>
+    <span class="hidden" id="logged">
+      <span>Bienvenid@ <span id="nombreUsuario" class="font-bold" /></span>
+
+      <button class="btn btn-sm variant-ghost-surface" on:click={logout}>
+        Logout
+      </button>
+    </span>
+    <span class="hidden" id="guest">
+      <a class="btn btn-sm variant-ghost-surface" href="/afiliacion">
+        Afiliarse
+      </a>
+      <a class="btn btn-sm variant-ghost-surface" href="/login"> Login </a>
+    </span>
   </svelte:fragment>
 </AppBar>
